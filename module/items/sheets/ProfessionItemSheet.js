3f93e9c29d336bdaad187b2b2cb2d4b9e374f95f
Никита Белов <n.belov@sberauto.com>
2022-07-29 17:40:06 +0300

    Дерево навыков профессии

--- module/items/sheets/ProfessionItemSheet.js
+++ module/items/sheets/ProfessionItemSheet.js
@@ -1,29 +1,14 @@
 import { WitcherBaseItemSheet } from "../BaseItemSheet.js"
 
-
 export class ProfessionItemSheet extends WitcherBaseItemSheet {
   
   /** @override */
   static get defaultOptions() {
     return mergeObject(super.defaultOptions, {
-      classes: ["witcher", "sheet", "item"],
-      width: 500,
-      height: 250,
+      width: 800,
+      height: 700,
     });
   }
-
-  getData(options) {
-    const data = super.getData(options);
-    console.log(data)
-    const itemData = data.data;
-    data.config = CONFIG.WITCHER;
-
-    // Re-define the template data references (backwards compatible)
-    data.item = itemData;
-    data.data = itemData.data;
-    return data;
-  }
-
 }
 
 
