6b792c7868b427e0fe7b7f69948adf6bac29b25d
Никита Белов <n.belov@sberauto.com>
2022-07-29 22:19:22 +0300

    Компендум заклинаний

--- tools/xlsx2json.py
+++ tools/xlsx2json.py
@@ -20,7 +20,15 @@ args = parser.parse_args()
 book = openpyxl.load_workbook(args.xlsx)
 sheet = book.active
 
-spells = []
+spells = {}
+spells['magicspell'] = []
+spells['invocDruid'] = []
+spells['invocPriest'] = []
+spells['InvocHighPriest'] = []
+spells['signs'] = []
+spells['ritual'] = []
+spells['injury'] = []
+
 
 for col in range(2, 999):
     
@@ -37,27 +45,161 @@ for col in range(2, 999):
     if costStamina in ["", None]:
         costStamina = 0
 
-    spells.append({
-        "name": sheet.cell(row=1, column=col).value,
-        "img": sheet.cell(row=5, column=col).value,
-        "type": "spell",
-        "data": {
-            "costStamina": costStamina,
-            "source": "core",
-            "description": sheet.cell(row=3, column=col).value,
-            "category": "magicspell",
-            "components": [],
-            "specData": {
-                "element": sheet.cell(row=8, column=col).value,
-                "LevelType": sheet.cell(row=9, column=col).value,
-                "DistantionType": sheet.cell(row=10, column=col).value,
-                "DistantionValue": distantion_value,
-                "defence": sheet.cell(row=12, column=col).value,
-                "duration": sheet.cell(row=13, column=col).value
-            }
-        }
-    })
+    category = sheet.cell(row=4, column=col).value
+    if category == 'spells':
+        category = 'magicspell'
+    
+    img = sheet.cell(row=5, column=col).value
+    if img != "" and img is not None:
+        img = "systems/TheWitcherRPG/assets%s" % img
 
-cf = open('spells.json', 'w', encoding='utf-8')
-cf.writelines(json.dumps(spells, ensure_ascii=False, indent=4, sort_keys=True))
-cf.close()
+    if category == 'magicspell':
+        spells['magicspell'].append({
+            "name": sheet.cell(row=1, column=col).value,
+            "img": img,
+            "type": "spell",
+            "data": {
+                "costStamina": costStamina,
+                "source": "core",
+                "effect": sheet.cell(row=3, column=col).value,
+                "category": category,
+                "components": [],
+                "specData": {
+                    "element": sheet.cell(row=8, column=col).value,
+                    "LevelType": sheet.cell(row=9, column=col).value,
+                    "DistantionType": sheet.cell(row=10, column=col).value,
+                    "DistantionValue": distantion_value,
+                    "defence": sheet.cell(row=12, column=col).value,
+                    "duration": sheet.cell(row=13, column=col).value
+                }
+            }
+        })
+    
+    if category == 'signs':
+        spells[category].append({
+            "name": sheet.cell(row=1, column=col).value,
+            "img": img,
+            "type": "spell",
+            "data": {
+                "costStamina": costStamina,
+                "source": "core",
+                "effect": sheet.cell(row=3, column=col).value,
+                "category": category,
+                "components": [],
+                "specData": {
+                    "element": sheet.cell(row=8, column=col).value,
+                    "LevelType": sheet.cell(row=9, column=col).value,
+                    "DistantionType": sheet.cell(row=10, column=col).value,
+                    "DistantionValue": distantion_value,
+                    "defence": sheet.cell(row=12, column=col).value,
+                    "duration": sheet.cell(row=13, column=col).value
+                }
+            }
+        })
+    
+    if category in ['invocPriest', 'invocDruid']:
+        spells[category].append({
+            "name": sheet.cell(row=1, column=col).value,
+            "img": img,
+            "type": "spell",
+            "data": {
+                "costStamina": costStamina,
+                "source": "core",
+                "effect": sheet.cell(row=3, column=col).value,
+                "category": category,
+                "components": [],
+                "specData": {
+                    #"element": sheet.cell(row=8, column=col).value,
+                    "LevelType": sheet.cell(row=9, column=col).value,
+                    "DistantionType": sheet.cell(row=10, column=col).value,
+                    "DistantionValue": distantion_value,
+                    "defence": sheet.cell(row=12, column=col).value,
+                    "duration": sheet.cell(row=13, column=col).value
+                }
+            }
+        })
+    
+    if category in ['ritual']:
+        spells[category].append({
+            "name": sheet.cell(row=1, column=col).value,
+            "img": img,
+            "type": "spell",
+            "data": {
+                "costStamina": costStamina,
+                "source": "core",
+                "effect": sheet.cell(row=3, column=col).value,
+                "category": category,
+                "components": [],
+                "specData": {
+                    #"element": sheet.cell(row=8, column=col).value,
+                    "preparationTime" :  sheet.cell(row=9, column=col).value,
+                    "LevelType": sheet.cell(row=12, column=col).value,
+                    "forgeDC": sheet.cell(row=10, column=col).value,
+                    #"DistantionType": sheet.cell(row=10, column=col).value,
+                    #"DistantionValue": distantion_value,
+                    #"defence": ,
+                    "duration": sheet.cell(row=11, column=col).value
+                }
+            }
+        })
+
+    if category in ['injury']:
+        spells[category].append({
+            "name": sheet.cell(row=1, column=col).value,
+            "img": img,
+            "type": "spell",
+            "data": {
+                "costStamina": costStamina,
+                "source": "core",
+                "effect": sheet.cell(row=3, column=col).value,
+                "category": category,
+                "components": [],
+                "specData": {
+                    "hazard": sheet.cell(row=8, column=col).value,
+                    "withdrawal": sheet.cell(row=9, column=col).value,
+                    #"element": sheet.cell(row=8, column=col).value,
+                    #"preparationTime" :  sheet.cell(row=9, column=col).value
+                    #"LevelType": sheet.cell(row=12, column=col).value,
+                    #"forgeDC": sheet.cell(row=10, column=col).value,
+                    #"DistantionType": sheet.cell(row=10, column=col).value,
+                    #"DistantionValue": distantion_value,
+                    #"defence": ,
+                    #"duration": sheet.cell(row=11, column=col).value
+                }
+            }
+        })
+
+    if category in ['InvocHighPriest']:
+        spells[category].append({
+            "name": sheet.cell(row=1, column=col).value,
+            "img": img,
+            "type": "spell",
+            "data": {
+                "costStamina": costStamina,
+                "source": "core",
+                "effect": sheet.cell(row=3, column=col).value,
+                "category": category,
+                "components": [],
+                "specData": {
+                    #"hazard": sheet.cell(row=8, column=col).value,
+                    #"withdrawal": sheet.cell(row=9, column=col).value,
+                    #"element": sheet.cell(row=8, column=col).value,
+                    #"preparationTime" :  sheet.cell(row=9, column=col).value
+                    "LevelType": sheet.cell(row=9, column=col).value,
+                    #"forgeDC": sheet.cell(row=10, column=col).value,
+                    #"DistantionType": sheet.cell(row=10, column=col).value,
+                    #"DistantionValue": distantion_value,
+                    #"defence": ,
+                    "deities": "",
+                    "duration": sheet.cell(row=13, column=col).value
+                }
+            }
+        })
+    
+
+    
+
+for key in spells:
+    cf = open('tmp/%s.json' % key, 'w', encoding='utf-8')
+    cf.writelines(json.dumps(spells[key], ensure_ascii=False, indent=4, sort_keys=True))
+    cf.close()
